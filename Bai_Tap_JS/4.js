e6dfc9df123839465717f59368c3e775383c1355
MTriet2301 <trietnguyen1892@gmail.com>
2024-06-06 14:06:15 +0700

    Update_4.js

--- Bai_Tap_JS/4.js
+++ Bai_Tap_JS/4.js
@@ -1,30 +1,149 @@
-// console.log(window);
-// console.log(window.innerHeight);
-// console.log(window.innerWidth);
+// // console.log(window);
+// // console.log(window.innerHeight);
+// // console.log(window.innerWidth);
 
-// const infoUser = {
-//     name : "Nguyễn Minh Triết",
-//     renderName : () =>{
-//         console.log("OK");
-//     } 
+// // const infoUser = {
+// //     name : "Nguyễn Minh Triết",
+// //     renderName : () =>{
+// //         console.log("OK");
+// //     } 
+// // };
+
+// // console.log(infoUser.name);
+// // console.log(infoUser.renderName());
+// var tabWindow;
+
+// const openTab = () => {
+//     tabWindow = window.open("https://simplemind.eu/faq/preferences/", "_blank", "width = 1200, height = 600, left = 100, top = 50");
 // };
 
-// console.log(infoUser.name);
-// console.log(infoUser.renderName());
-var tabWindow;
+// const closeTab = () => {
+//     tabWindow.close();
+// }
 
-const openTab = () => {
-    tabWindow = window.open("https://simplemind.eu/faq/preferences/", "_blank", "width = 1200, height = 600, left = 100, top = 50");
-};
+// const Reload = () => {
+//     location.reload();
+// }
+// // console.log(screen.height);
+// // console.log(screen.width);s
+// // console.log(navigator.platform); 
+// // console.log(navigator.appName);
 
-const closeTab = () => {
-    tabWindow.close();
-}
+// // const fullName = prompt("Nhập tên của bạn");
+// // console.log(fullName);
 
-const Reload = () => {
-    location.reload();
-}
+// // document.cookie = fullName;
+// console.log(document.cookie.split(" "));
+// console.log(location.href);
+
+// const deleteCookie = (cname) => {
+//     document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+// }
+// x
+// deleteCookie = ("Name");s
+
+// Câu 01
+
+// console.log(location.href);
+
+// Câu 02;
+
+// console.log(location.protocol);
+// console.log(location.hostname);
+// console.log(location.port);
+
+
+// Câu 03
+
+// const Divert = (url, delay) => {
+//     setTimeout( function()  {
+//         window.location.href = url;
+//     }, delay);
+// }
+
+// Divert("https://drive.google.com/drive/u/1/folders/1QrgL2OMgs3NNUphF1_Y4Dsv5xQRxWaVt", 3000);
+
+// Câu 04
+
+// const RefreshPage = (time) => {
+//     setInterval(() => {
+//         location.reload();
+//     }, time);
+// }
+
+// RefreshPage(3000);
+
+// Câu 05
+
+// const CheckProtocol = () => {
+//     if(location.protocol === "https: "){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(CheckProtocol());
+// // console.log(location);
+
+// Câu 06
+// window.print();
+
+// Câu 07
+// var Seconds = 10;
+
+// const CountDown = () => {
+//     if(Seconds >= 0){
+//         console.log(Seconds);
+//         Seconds--;
+//         setTimeout(CountDown, 1000);
+//     }
+//     else{
+//         console.log("Time is out");
+//     }
+// }
+// CountDown();
+
+// Câu 08
+// console.log(innerHeight);
+// console.log(innerWidth);
+
+// Câu 9;
+
 // console.log(screen.height);
 // console.log(screen.width);
-console.log(navigator.platform); 
-console.log(navigator.appName);
+
+// // Câu 10
+// window.open("https://whitehat.vn/", "", "height = 2000, width = 1900");
+
+// Câu 11
+
+// console.log(navigator.userAgent);
+
+// Câu 12
+
+// console.log(navigator.language);
+
+// Câu 13
+// console.log(location);
+// console.log(navigator);
+//
+
+// Câi 14
+// const CheckOs = () => {
+//     if(navigator.userAgentData.mobile){
+//         console.log("Mobile");
+//     }
+//     else{
+//         console.log("PC");
+//     }
+// } 
+
+// CheckOs();
+
+// Câu 15
+
+// console.log(navigator.languages);
+
+
